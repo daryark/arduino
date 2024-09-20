e1dc5183370474a3d54f2be0a71111698d360ce6
Dasha Yarkovska <yarkovska11@gmail.com>
2024-09-20 15:05:04 +0200

    Update arduino_proj1_bonus2.ino

--- arduino_proj1_bonus2/arduino_proj1_bonus2.ino
+++ arduino_proj1_bonus2/arduino_proj1_bonus2.ino
@@ -1,7 +1,7 @@
-#define SWITCH_BTN 2
-#define GREEN_LED 12
-#define YELLOW_LED 8
-#define RED_LED 13
+#define SWITCH_BTN 7
+#define GREEN_LED 9
+#define YELLOW_LED 10
+#define RED_LED 11
 void setup() {
   pinMode(SWITCH_BTN, INPUT_PULLUP);
   pinMode(RED_LED, OUTPUT);
@@ -9,27 +9,36 @@ void setup() {
   pinMode(YELLOW_LED, OUTPUT);
   digitalWrite(GREEN_LED, HIGH);
 }
-unsigned long previous = 0;
+unsigned long prevYellow = 0;
+unsigned long prevRed = 0;
 const long intervalRed = 500;
 const long intervalYellow = 2000;
 
 void loop() {
   unsigned long current = millis();
   if (digitalRead(SWITCH_BTN) == HIGH) {
-    previous = 0;
+    prevRed = 0;
+    prevYellow = 0;
 
     digitalWrite(RED_LED, LOW);
     digitalWrite(YELLOW_LED, LOW);
     digitalWrite(GREEN_LED, HIGH);
   } else {
     digitalWrite(GREEN_LED, LOW);
-    if (!digitalRead(RED_LED) && !digitalRead(YELLOW_LED))
-      digitalWrite(RED_LED, HIGH);
-    if ((current - previous >= intervalRed && digitalRead(RED_LED)
-      || (current - previous >= intervalYellow && digitalRead(YELLOW_LED)))) {
-      previous = current;
-      digitalWrite(YELLOW_LED, !digitalRead(YELLOW_LED)); 
+    if (current - prevRed >= intervalRed )
+    {   
+      prevRed = current;
       digitalWrite(RED_LED, !digitalRead(RED_LED));
+      delay(100);
+      digitalWrite(RED_LED, !digitalRead(RED_LED));
+
+    }
+    if (current - prevYellow >= intervalYellow ) {
+      prevYellow = current;
+      digitalWrite(YELLOW_LED, !digitalRead(YELLOW_LED));
+      delay(100);
+      digitalWrite(YELLOW_LED, !digitalRead(YELLOW_LED));
+
     }
   }
 }
